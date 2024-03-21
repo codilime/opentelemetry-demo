c5c1525d7c1c05a4be0b9ee1eebb6d11f4bbffe1
Jakub Michalak <jakub.michalak@codilime.com>
2024-03-21 12:27:44 +0100

    Use codilime fork

--- src/a.sh
+++ src/a.sh
@@ -4,5 +4,5 @@
 for file in *; do
     NAME="$(basename $file service)"
     # Copy a template YAML file to the new directory
-    sed -i "s|jmichalak9/opentelemetry.io/|jmichalak9/opentelemetry.io/tree/main/content/en/docs/demo/services/$NAME|g" $file/backstage.yaml #> "$file/backstage.yaml"
+    sed -i "s|codilime/opentelemetry.io/|codilime/opentelemetry.io/tree/main/content/en/docs/demo/services/$NAME|g" $file/backstage.yaml #> "$file/backstage.yaml"
 done
